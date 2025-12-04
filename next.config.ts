83cb661511e9a7bcba1cea159b65c1f135bf71f0
Longlo2k3 <longch1@tnteco.vn>
2025-12-04 10:01:50 +0700

    cau hinh file config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
   },
   env: {
     NEXT_PUBLIC_URL_API_SERVER: "https://apiesenglish-y99l.onrender.com/api",
+    NEXT_PUBLIC_GEMINI_API_KEY: "AIzaSyDtaqMEOiBa4qF62ImxjyeTwg3fWCjx_z8",
   },
 };
 
