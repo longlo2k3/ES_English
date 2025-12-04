3d703e4d0ed92b03a222364f37df08684120cbc1
Longlo2k3 <longch1@tnteco.vn>
2025-12-04 10:22:29 +0700

    thay doi key api cua gemini

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   },
   env: {
     NEXT_PUBLIC_URL_API_SERVER: "https://apiesenglish-y99l.onrender.com/api",
-    NEXT_PUBLIC_GEMINI_API_KEY: "AIzaSyAFgNAKx0CLZ0MAai0iAfrEqPinPbNyz90",
+    NEXT_PUBLIC_GEMINI_API_KEY: "AIzaSyBrzjD61AGiY3esZTmTTpcgdoQ0Thvh-3Q",
   },
 };
 
--- ts-framework/ts-module-chatbot/hooks/gemini.js
+++ ts-framework/ts-module-chatbot/hooks/gemini.js
@@ -1,8 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const NEXT_PUBLIC_GEMINI_API_KEY = "AIzaSyDtaqMEOiBa4qF62ImxjyeTwg3fWCjx_z8";
-
-const genAI = new GoogleGenerativeAI(NEXT_PUBLIC_GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 
 export async function callGeminiAPI(prompt) {
   try {
